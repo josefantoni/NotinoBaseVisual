a1ed68d7ef39db93a41718dda1a31cf46ced6b1a
Josef Antoni <antoni.josef@icloud.com>
2022-06-13 15:10:13 +0200

    New Xcode project created, added cocoapods with Alamofire networking

---
+++ NotinoBaseVisual/NotinoBaseVisualApp.swift
@@ -0,0 +1,20 @@
+//
+//  NotinoBaseVisualApp.swift
+//  NotinoBaseVisual
+//
+//  Created by Josef Antoni on 09.06.2022.
+//
+
+import SwiftUI
+import Combine
+
+@main
+struct NotinoBaseVisualApp: App {
+    init() {
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+        }
+    }
+}
