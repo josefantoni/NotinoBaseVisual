c5f707f8a3d0f9cea06288721ac25ecb1e418941
Josef Antoni <antoni.josef@icloud.com>
2022-06-17 14:43:54 +0200

    1) Implemented api response with COMBINE solution 2) changed to protocol Codable for just cleaner look

--- NotinoBaseVisual/ViewModel/BoardViewModel.swift
+++ NotinoBaseVisual/ViewModel/BoardViewModel.swift
@@ -9,11 +9,42 @@ import Foundation
 import Combine
 
 class BoardViewModel: ObservableObject {
+    private var bag = Set<AnyCancellable>()
+
     @Published var products: [CatalogProduct] = []
     @Published var favouriteProductsbyId: [Int] = []
 
     func fetchData() {
+        guard let targetUrl = URL(string: Strings.productsUrl) else {
+            print("Error not a valid URL")
+            return
+        }
         
+        URLSession.shared.dataTaskPublisher(for: targetUrl)
+            .receive(on: DispatchQueue.main)
+            .tryMap(handleReceivedOutput)
+            .decode(type: CatalogProductsResponse.self, decoder: JSONDecoder())
+            // IF for any reason this failed. For presentation i am giving here a mock data -> replaceEmpty
+            .replaceEmpty(with: CatalogProductsResponse(catalogProducts: MockProducts.products))
+            .sink { (completion) in
+                switch completion {
+                case .finished:
+                    print("New catalog was downloaded!")
+                case .failure(let error):
+                    print("Error occured while downloading a new catalog: \(error)")
+                }
+            } receiveValue: { [weak self] receivedProducts in
+                self?.products = receivedProducts.catalogProducts
+            }
+            .store(in: &bag)
+    }
+    
+    private func handleReceivedOutput(output: URLSession.DataTaskPublisher.Output) throws -> Data {
+        guard let response = output.response as? HTTPURLResponse,
+            response.statusCode >= 200 && response.statusCode < 300 else {
+                throw URLError(.badServerResponse)
+        }
+        return output.data
     }
     
     func markFavourite(product: CatalogProduct) {
@@ -30,10 +61,4 @@ class BoardViewModel: ObservableObject {
         return PersistentStore.retrieveFavouriteIndex(from: favouriteProductsbyId,
                                                       productById: product.productId) != nil
     }
-}
-
-struct CatalogProductsResponse: Decodable {
-   let catalogProducts : [CatalogProduct]
-
-   enum CodingKeys : String, CodingKey { case catalogProducts = "vpProductByIds" }
 }
